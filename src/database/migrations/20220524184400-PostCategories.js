67ba3d65a01db204784ad39e709767de0c2d68dd
Vinicius Jacques <vinicius-jacques@hotmail.com>
2022-05-24 16:20:57 -0300

    Requisito 1

---
+++ src/database/migrations/20220524184400-PostCategories.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const PostCategoriesTable = queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+    });
+
+    return PostCategoriesTable;
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.dropTable('PostCategories')
+  }
+};
